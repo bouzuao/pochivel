e01427e0187fd244d21414a1c731f8322f232bbf
= <=>
2014-02-13 23:01:12 +0900

    gushiken じゃらんAPIにてハッシュで整形中

--- lib/tasks/insert_contents_task.rb
+++ lib/tasks/insert_contents_task.rb
@@ -18,20 +18,19 @@ class Tasks::InsertContentsTask
     #TODO:テーブルにどこまでデータを保持するかを検討する必要がある。。
     #TODO:ある程度のサイトは一つのモジュールで対応可能なためパラメータや条件で取得を変えるようにしたい。
 
-    apis = ['http://webservice.recruit.co.jp/relax/salon/v1?response_reserve=1&count=15&order=3&address=%E9%8A%80%E5%BA%A7&key=d80cc5011c92e61d',
-            'http://webservice.recruit.co.jp/relax/salon/v1?response_reserve=1&count=15&order=3&address=%E6%B8%8B%E8%B0%B7&key=d80cc5011c92e61d'
-    ]
+    apis = ['http://jws.jalan.net/APIAdvance/StockSearch/V1/?key=aqr1442b809a0e&pref=010000&stay_count=3&adult_num=2&count=50']
     apis.each do |url|
       h = Hash.from_xml(open(url).read)
 
-      h['results']['salon'].each do |t|
-        pp t['urls']['pc']
-        pp t['mood'][0]['photo']
-        content= Content.new
-        content.image_url =  t['mood'][0]['photo']
-        content.url = t['urls']['pc']
-        content.genre = 4
-        content.save
+      h['Results']['Plan'].each do |t|
+        pp t
+        #pp t['urls']['pc']
+        #pp t['mood'][0]['photo']
+        #content= Content. new
+        #content.image_url =  t['mood'][0]['photo']
+        #content.url = t['urls']['pc']
+        #content.genre = 4
+        #content.save
 
       end
     end
