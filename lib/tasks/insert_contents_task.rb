7c9c0b2aaeccd7a9f3e07065dd7641cb5b04c9cd
= <=>
2014-02-15 14:11:23 +0900

    gushiken　都道府県別の50件分情報取得 nil対応は未対応

--- lib/tasks/insert_contents_task.rb
+++ lib/tasks/insert_contents_task.rb
@@ -18,19 +18,27 @@ class Tasks::InsertContentsTask
     #TODO:テーブルにどこまでデータを保持するかを検討する必要がある。。
     #TODO:ある程度のサイトは一つのモジュールで対応可能なためパラメータや条件で取得を変えるようにしたい。
 
-    apis = ['http://jws.jalan.net/APIAdvance/StockSearch/V1/?key=aqr1442b809a0e&pref=010000&stay_count=3&adult_num=2&count=50']
+    apis = []
+    for i in 1..47 do
+    str = "%02d" % i.to_s
+     pref = 'pref=' + str + '0000'
+    hotel_api = 'http://jws.jalan.net/APIAdvance/StockSearch/V1/?key=aqr1442b809a0e&'
+    pp hotel_api += pref
+    pp hotel_api += '&stay_count=2&adult_num=2&count=50'
+    apis << hotel_api
+    end
+
     apis.each do |url|
       h = Hash.from_xml(open(url).read)
-
       h['Results']['Plan'].each do |t|
         pp t['PlanPictureURL']
-        #pp t['mood'][0]['photo']
-        #content= Content. new
-        #content.image_url =  t['mood'][0]['photo']
-        #content.url = t['urls']['pc']
-        #content.genre = 4
-        #content.save
-
+        pp t['PlanPictureURL']
+    #    #pp t['mood'][0]['photo']
+    #    #content= Content. new
+    #    #content.image_url =  t['mood'][0]['photo']
+    #    #content.url = t['urls']['pc']
+    #    #content.genre = 4
+    #    #content.save
       end
     end
 
